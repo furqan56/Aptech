978ea8e2c52209735bc714de2edcf544522dd11b
Furqan Khan <furqankhan5566@gmail.com>
2019-01-10 14:48:44 +0500

    Add project files.

---
+++ SleeperCell/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SleeperCell.Startup))]
+namespace SleeperCell
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
