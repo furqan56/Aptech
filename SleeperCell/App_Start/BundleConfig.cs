0228f8de96b7e244087cd9f153f3db302b88cfaa
asmasyed10 <46635608+asmasyed10@users.noreply.github.com>
2019-01-16 13:47:52 +0500

    New Layout Added

--- SleeperCell/App_Start/BundleConfig.cs
+++ SleeperCell/App_Start/BundleConfig.cs
@@ -16,15 +16,26 @@ namespace SleeperCell
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/adminLTE").Include(
+                        "~/Scripts/AdminLTE-JS/adminlte.min.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/adminLTE/css/AdminLTE.min.css",
+                      "~/Content/font-awesome/css/font-awesome.min.css",
+                      "~/Content/Ionicons/css/ionicons.min.css",
+                      "~/Content/adminLTE/css/skins/skin-blue.min.css"
+                      ));
         }
     }
 }
