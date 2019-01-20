0d9fed4e384fadfd1a03607f9cee01bdd1c5f422
asmasyed10 <46635608+asmasyed10@users.noreply.github.com>
2019-01-21 01:11:40 +0500

    Create and Update View Added

--- SleeperCell/Controllers/ProductController.cs
+++ SleeperCell/Controllers/ProductController.cs
@@ -3,23 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SleeperCell.Handlers;
 using SleeperCell.Models;
 namespace SleeperCell.Controllers
 {
     public class ProductController : Controller
     {
-             
-            // GET: Products
+        private ProductRepository  _productRepository;
+        // GET: Products
+        public ProductController()
+        {
+            _productRepository = new ProductRepository();
+        }
+            
         public ActionResult Index()
         {
-            
-
-            
-
-
-            
+            return View(_productRepository.GetAllProducts());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
             return View();
         }
-        
+        [HttpPost]
+        public ActionResult Create(ProductViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                _productRepository.AddProduct(model);
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+        [HttpPut]
+        public ActionResult Update(ProductViewModel model)
+        {
+
+            _productRepository.Update(model);
+
+            return View(model);
+        }
+
+        //public ActionResult Delete(ProductViewModel model)
+        //{
+        //    _productRepository.Delete(model);
+
+        //    return View();
+        //}
     }
 }
--- SleeperCell/Handlers/ProductRepository.cs
+++ SleeperCell/Handlers/ProductRepository.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SleeperCell.Models;
 
 namespace SleeperCell.Handlers
 {
     public class ProductRepository
     {
-        public int List<ProductViewModel> ProductsStore { get; set; } = new List<ProductViewModel>
+        public static List<ProductViewModel> ProductsStore { get; set; } = new List<ProductViewModel>();
+        public static int ProductId { get; set; } = 1;
         public List<ProductViewModel> GetAllProducts()
         {
-            
+            return ProductsStore;
+        }
+
+        public void AddProduct(ProductViewModel model)
+        {
+            model.ID = ProductId++;
+            ProductsStore.Add(model);
+        }
+
+        public void Update(ProductViewModel model)
+        {
+            ProductsStore.RemoveAll(p => p.ID == model.ID);
+            ProductsStore.Add(model);
+        }
+
+        public void Delete(int id)
+        {
+            ProductsStore.RemoveAll(p => p.ID == id);
         }
     }
 }
