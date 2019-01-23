e33165a82c868fb4a68397770e849b14ef6da953
BwpComputers <BwpComputers@BWPCOMPUTER>
2019-01-23 15:01:39 +0500

    order

---
+++ SleeperCell/Handlers/orderRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SleeperCell.Models;
+
+namespace SleeperCell.Handlers
+{
+    public class orderRepository
+    {
+
+        public static List<orderViewModel> ordersStore { get; private set; } = new List<orderViewModel>();
+        public static int orderId { get; private set; } = 1;
+        public List<orderViewModel> GetAllorders()
+        {
+            return ordersStore;
+        }
+
+        public void Addorder(orderViewModel model)
+        {
+            model.Id = orderId++;
+            ordersStore.Add(model);
+        }
+
+        public void Update(orderViewModel model)
+        {
+            ordersStore.RemoveAll(p => p.Id == model.Id);
+            ordersStore.Add(model);
+        }
+        public void Delete(int id)
+        {
+            ordersStore.RemoveAll(p => p.Id == id);
+        }
+    }
+}
---
+++ SleeperCell/Models/orderViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace SleeperCell.Models
+{
+    public class orderViewModel
+    {
+        public int Id { get; set; }
+        [DisplayName("Product Name")]
+        public string ProductName { get; set; }
+        [DisplayName("Product Quantity")]
+        public decimal ProductQuantity { get; set; }
+        [DisplayName("Product Description")]
+        public string PDescription { get; set; }
+       
+    }
+}
