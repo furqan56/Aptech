324c756c4dfaf72f34d0d906ccebe13e2638fe75
Abdul Rehman Aftab <abdulrehman.pro@live.com>
2019-01-25 16:23:20 +0500

    Added Migrations, object models and dbset contexts

---
+++ SleeperCell/ObjectModel/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SleeperCell.ObjectModel
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Barcode { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double UnitCost { get; set; }
+        public double UnitPrice { get; set; }
+        public int QuantityInHand { get; set; }
+        public Category Category { get; set; }
+    }
+}
