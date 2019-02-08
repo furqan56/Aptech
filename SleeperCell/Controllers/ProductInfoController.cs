50a27b558197d0916befca17c48ecdfeb30b6a7b
Abdul Rehman Aftab <abdulrehman.pro@live.com>
2019-02-08 16:46:23 +0500

    Write ajax call with controller

---
+++ SleeperCell/Controllers/ProductInfoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SleeperCell.Models;
+
+namespace SleeperCell.Controllers
+{
+    [RoutePrefix("Product")]
+    public class ProductInfoController : Controller
+    {
+        // GET: ProductInfo
+        [HttpGet,Route("info/{start}/{count}")]
+        public ActionResult Index(int start,int count)
+        {
+            if (Request.IsAjaxRequest())
+            {
+                var data=new List<ProductViewModel>();
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+
+            return View();
+        }
+    }
+}
