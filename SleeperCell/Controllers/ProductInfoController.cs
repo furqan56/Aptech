7ac406788006013c2d12a518ba30f6b0b8a1865d
Abdul Rehman Aftab <abdulrehman.pro@live.com>
2019-02-11 16:26:36 +0500

    Write POST call on product create and corrected the route config

--- SleeperCell/Controllers/ProductInfoController.cs
+++ SleeperCell/Controllers/ProductInfoController.cs
@@ -1,28 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Linq;
 using SleeperCell.Models;
 using SleeperCell.Handlers;
+using System.Web.Mvc;
 
 namespace SleeperCell.Controllers
 {
-    [RoutePrefix("Product")]
+    [RoutePrefix("product")]
     public class ProductInfoController : Controller
     {
         ProductRepository _productRepository = new ProductRepository(); 
         // GET: ProductInfo
-        [HttpGet,Route("info/{start}/{count}")]
-        public ActionResult Index(int start,int count)
+        [HttpGet, Route("info/{start}/{count}")]
+        public ActionResult Index(int start, int count)
         {
             if (Request.IsAjaxRequest())
             {
-                var data = _productRepository.GetAllProducts().OrderByDescending(x => x.Id).Take(count);
+                var data = _productRepository.GetAllProducts().OrderByDescending(x => x.Id).Skip(start).Take(count);
                 return Json(data, JsonRequestBehavior.AllowGet);
             }
 
             return View();
+        }
+
+        [HttpPost, Route("create")]
+        public ActionResult Create(ProductViewModel model)
+        {
+            if (Request.IsAjaxRequest())
+            {
+                var response = new
+                {
+                    Status = 200,
+                    StatusText = "OK"
+                };
+                return Json(response);
+            }
+            return RedirectToAction("Create", "Products");
+        }
+
+        [HttpPost]
+        public ActionResult Test(string name)
+        {
+            var response = new
+            {
+                Status = 200,
+                StatusText = "OK"
+            };
+            return Json(response);
         }
     }
 }
