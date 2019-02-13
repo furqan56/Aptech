0698fbe850dc1d5d9757ba47ea286588d4dc25d1
Afnan khan <afakhan413@gmail.com>
2019-02-13 22:41:07 +0500

    m

--- SleeperCell/Models/ProductStockViewMOdel.cs
+++ SleeperCell/Models/ProductStockViewMOdel.cs
@@ -12,10 +12,9 @@ namespace SleeperCell.Models
         public string Vendor { get; set; }
         public double UnitPrice { get; set; }
         public int ProductId { get; set; }
-        public string ProductName { get; set; }
-        public string Vendor { get; set; }
+     
         public DateTime CreationDate { get; set; }
-        public int ProductId { get; set; }
+      
         public int VendorId { get; set; }
         public string Category { get; set; }
     }
