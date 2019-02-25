0673669a538da574b30097fcdc0558ac792b31a1
Abdul Rehman Aftab <abdulrehman.pro@live.com>
2019-02-25 15:28:19 +0500

    Added Products api controller

---
+++ SleeperCell/ApiControllers/CategoryController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SleeperCell.Context;
+using SleeperCell.Models;
+
+namespace SleeperCell.ApiControllers
+{
+    //public class CategoryController : ApiController
+    //{
+    //    private SleeperCellContext db = new SleeperCellContext();
+
+    //    // GET: api/Category
+    //    public IQueryable<CategoryViewModel> GetCategoryViewModels()
+    //    {
+    //        return db.CategoryViewModels;
+    //    }
+
+    //    // GET: api/Category/5
+    //    [ResponseType(typeof(CategoryViewModel))]
+    //    public IHttpActionResult GetCategoryViewModel(int id)
+    //    {
+    //        CategoryViewModel categoryViewModel = db.CategoryViewModels.Find(id);
+    //        if (categoryViewModel == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        return Ok(categoryViewModel);
+    //    }
+
+    //    // PUT: api/Category/5
+    //    [ResponseType(typeof(void))]
+    //    public IHttpActionResult PutCategoryViewModel(int id, CategoryViewModel categoryViewModel)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return BadRequest(ModelState);
+    //        }
+
+    //        if (id != categoryViewModel.Id)
+    //        {
+    //            return BadRequest();
+    //        }
+
+    //        db.Entry(categoryViewModel).State = EntityState.Modified;
+
+    //        try
+    //        {
+    //            db.SaveChanges();
+    //        }
+    //        catch (DbUpdateConcurrencyException)
+    //        {
+    //            if (!CategoryViewModelExists(id))
+    //            {
+    //                return NotFound();
+    //            }
+    //            else
+    //            {
+    //                throw;
+    //            }
+    //        }
+
+    //        return StatusCode(HttpStatusCode.NoContent);
+    //    }
+
+    //    // POST: api/Category
+    //    [ResponseType(typeof(CategoryViewModel))]
+    //    public IHttpActionResult PostCategoryViewModel(CategoryViewModel categoryViewModel)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return BadRequest(ModelState);
+    //        }
+
+    //        db.CategoryViewModels.Add(categoryViewModel);
+    //        db.SaveChanges();
+
+    //        return CreatedAtRoute("DefaultApi", new { id = categoryViewModel.Id }, categoryViewModel);
+    //    }
+
+    //    // DELETE: api/Category/5
+    //    [ResponseType(typeof(CategoryViewModel))]
+    //    public IHttpActionResult DeleteCategoryViewModel(int id)
+    //    {
+    //        CategoryViewModel categoryViewModel = db.CategoryViewModels.Find(id);
+    //        if (categoryViewModel == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        db.CategoryViewModels.Remove(categoryViewModel);
+    //        db.SaveChanges();
+
+    //        return Ok(categoryViewModel);
+    //    }
+
+    //    protected override void Dispose(bool disposing)
+    //    {
+    //        if (disposing)
+    //        {
+    //            db.Dispose();
+    //        }
+    //        base.Dispose(disposing);
+    //    }
+
+    //    private bool CategoryViewModelExists(int id)
+    //    {
+    //        return db.CategoryViewModels.Count(e => e.Id == id) > 0;
+    //    }
+    //}
+}
---
+++ SleeperCell/ApiControllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using SleeperCell.Handlers;
+using SleeperCell.Models;
+
+namespace SleeperCell.ApiControllers
+{
+    public class ProductsController : ApiController
+    {
+        private ProductService _productService;
+        private CategoryService _categoryService;
+        public ProductsController()
+        {
+            _productService = new ProductService();
+            _categoryService = new CategoryService();
+        }
+        [HttpGet]
+        public IHttpActionResult Index()
+        {
+            var products = _productService.GetAllProducts();
+
+            return Json(products);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Index([FromBody] ProductViewModel product)
+        {
+            _productService.AddProduct(product);
+
+            return Ok();
+        }
+
+        [HttpGet, Route("find/{id}")]
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = _productService.FindProduct(id);
+
+            return Json(product);
+        }
+    }
+}
